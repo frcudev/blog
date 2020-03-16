7f58ed8f5784b53be41bcfbb40f4fd3561419882
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-03-16 18:52:22 -0300

    feat: 🎸 add folder structure

--- projects/blog/src/app/app.module.ts
+++ projects/blog/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CoreModule} from './core/core.module';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ projects/blog/src/app/features/home/home.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+
+import {HomeRoutingModule} from './home-routing.module';
+import {SharedModule} from '../../shared/shared.module';
+
+import {HomeComponent} from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [SharedModule, HomeRoutingModule],
+})
+export class HomeModule {}
