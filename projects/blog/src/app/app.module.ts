2ca5cb1feb7ff99d945a6be2e6259595a6fe5672
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-03-20 17:40:08 -0300

    feat: 🎸 add @angular/fire

--- projects/blog/src/app/app.module.ts
+++ projects/blog/src/app/app.module.ts
@@ -4,8 +4,13 @@ import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core/core.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import {LoadingService} from './core/services/loading.service';
+
+import { environment } from '../environments/environment';
 
 import {AppComponent} from './app.component';
 
@@ -15,6 +20,11 @@ import {AppComponent} from './app.component';
     // vendor
     BrowserAnimationsModule,
     CommonModule,
+
+    // firebase
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
 
     // local
     CoreModule,
