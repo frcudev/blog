d46747e52dbfeb9392f6be1bf5c2b826cb223da2
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-03-22 14:02:58 -0300

    feat: 🎸 add material modules and reactive forms module

--- projects/blog/src/app/shared/shared.module.ts
+++ projects/blog/src/app/shared/shared.module.ts
@@ -2,13 +2,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {ReactiveFormsModule} from '@angular/forms';
 
-const MATERIAL_MODULES = [MatIconModule, MatButtonModule];
+const MATERIAL_MODULES = [MatIconModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FlexLayoutModule, ...MATERIAL_MODULES],
-  exports: [CommonModule, FlexLayoutModule, ...MATERIAL_MODULES],
+  imports: [CommonModule, FlexLayoutModule, ReactiveFormsModule, ...MATERIAL_MODULES],
+  exports: [CommonModule, FlexLayoutModule, ReactiveFormsModule, ...MATERIAL_MODULES],
 })
 export class SharedModule {}
