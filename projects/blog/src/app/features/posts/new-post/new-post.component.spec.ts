177371b65f217be585ace004c30dacb4fc4aa112
Facundo Rodriguez <rodriguez.facu23@gmail.com>
2020-03-22 14:06:33 -0300

    fix: 🐛 isolate new post tests

--- projects/blog/src/app/features/posts/new-post/new-post.component.spec.ts
+++ projects/blog/src/app/features/posts/new-post/new-post.component.spec.ts
@@ -1,3 +1,4 @@
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NewPostComponent} from './new-post.component';
@@ -9,6 +10,7 @@ describe('NewPostComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NewPostComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
